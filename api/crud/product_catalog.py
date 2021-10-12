5ac05fc6f808db0ea5e289a93a25080ee4880acd
igol84 <igolub84@gmail.com>
2021-10-12 16:40:17 +0300

    pc

--- api/crud/product_catalog.py
+++ api/crud/product_catalog.py
@@ -1,3 +1,4 @@
+from sqlalchemy import or_
 from .. import tables
 from ..schemas import product_catalog as schemas
 from .base import CRUDBase
@@ -7,3 +8,22 @@ class ProductCatalog(CRUDBase[tables.ProductCatalog, schemas.CreateRowProductCat
     table = tables.ProductCatalog
     keys = ['store_id', 'prod_id']
     autoincrement = False
+
+    def get_store_pc(
+            self, store_id: int, skip: int = 0, limit: int = None,
+            q_search: str = None) -> list[tables.ProductCatalog]:
+        if limit:
+            if skip:
+                SLICE = slice(skip, skip + limit)
+            else:
+                SLICE = slice(skip, limit)
+        else:
+            SLICE = slice(skip, None)
+        db_obj = self.db.query(self.table).filter(self.table.store_id == store_id)
+
+        if self.search_columns and q_search:
+            search = []
+            for column in self.search_columns:
+                search.append(getattr(self.table, column).ilike(f'%{q_search}%'))
+            db_obj = db_obj.filter(or_(*search))
+        return db_obj[SLICE]
--- api/routers/product_catalog.py
+++ api/routers/product_catalog.py
@@ -13,8 +13,8 @@ def create_row(request: schemas.CreateRowProductCatalog, product_catalog: crud.P
 
 
 @router.get('/', response_model=list[schemas.ShowRowProductCatalogWithProduct])
-def get_all_rows(skip: int = None, limit: int = None, search: str = None, sli: crud.ProductCatalog = Depends()):
-    return sli.get_all(skip, limit, search)
+def get_all_rows(skip: int = None, limit: int = None, search: str = None, crud_pc: crud.ProductCatalog = Depends()):
+    return crud_pc.get_all(skip, limit, search)
 
 
 @router.get('/{store_id}/{prod_id}')
@@ -24,7 +24,12 @@ def check_row(store_id: int, prod_id: int, crud_product_catalog: crud.ProductCat
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
+@router.get('/{store_id}', status_code=200, response_model=list[schemas.ShowRowProductCatalogWithProduct])
+def get_store_pc(store_id: int, skip: int = None, limit: int = None, crud_pc: crud.ProductCatalog = Depends()):
+    return crud_pc.get_store_pc(store_id, skip, limit)
+
+
 @router.delete('/{store_id}/{prod_id}')
-def delete_row(store_id: int, prod_id: int, product_catalog: crud.ProductCatalog = Depends()):
-    product_catalog.delete(store_id, prod_id)
+def delete_row(store_id: int, prod_id: int, crud_pc: crud.ProductCatalog = Depends()):
+    crud_pc.delete(store_id, prod_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
