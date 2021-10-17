74a35f51a7be50697b6213f1de582e25da607114
igol84 <igolub84@gmail.com>
2021-10-17 14:58:26 +0300

    update qty in items when we create, update, delete sale

--- api/crud/sale.py
+++ api/crud/sale.py
@@ -1,5 +1,7 @@
+from collections import Counter
 from .. import tables
 from ..schemas import sale as schemas_sale
+from ..schemas import item as schemas_item
 from .base import CRUDBase
 
 
@@ -10,6 +12,14 @@ class Sale(CRUDBase[tables.Sale, schemas_sale.CreateSale, schemas_sale.BaseSale]
         sale_row = tables.Sale(**request.dict(exclude={'sale_line_items'}))
         sale_row.sale_line_items = [tables.SaleLineItem(**pd_sli.dict()) for pd_sli in request.sale_line_items]
         self.db.add(sale_row)
+
+        # Update qty in items
+        for pd_sli in request.sale_line_items:
+            item_row = self.db.query(tables.Item).filter(tables.Item.id == pd_sli.item_id)
+            item_pd = schemas_item.Item.from_orm(item_row.first())
+            item_pd.qty -= pd_sli.qty
+            item_row.update(item_pd.dict())
+
         self.db.commit()
         self.db.refresh(sale_row)
         return sale_row
@@ -17,6 +27,23 @@ class Sale(CRUDBase[tables.Sale, schemas_sale.CreateSale, schemas_sale.BaseSale]
     def update(self, request: schemas_sale.UpdateSale) -> tables.Sale:
         sale_row = self._get(request.id)
         sli_rows = self.db.query(tables.SaleLineItem).filter(tables.SaleLineItem.sale_id == request.id)
+
+        # Update qty in items
+        dict_sli_s = {sli.item_id: sli.qty for sli in sli_rows}
+        dict_new_sli_s = {sli.item_id: sli.qty for sli in request.sale_line_items}
+        count_sli_s, count_new_sli_s = Counter(), Counter()
+        for item_id_, qty_ in dict_sli_s.items():
+            count_sli_s[item_id_] += qty_
+        for item_id_, qty_ in dict_new_sli_s.items():
+            count_new_sli_s[item_id_] += qty_
+        items_ids = list(dict_sli_s | dict_new_sli_s)
+        need_update_items = [item_id_ for item_id_ in items_ids if count_sli_s[item_id_] != count_new_sli_s[item_id_]]
+        for item_id_ in need_update_items:
+            item_row = self.db.query(tables.Item).filter(tables.Item.id == item_id_)
+            item_pd = schemas_item.Item.from_orm(item_row.first())
+            item_pd.qty += count_sli_s[item_id_] - count_new_sli_s[item_id_]
+            item_row.update(item_pd.dict())
+
         sli_rows.delete(synchronize_session=False)
         sale_row.update(request.dict(exclude={'sale_line_items'}))
         sale_line_items = [tables.SaleLineItem(sale_id=request.id, **pd_sli.dict()) for pd_sli in
@@ -27,7 +54,34 @@ class Sale(CRUDBase[tables.Sale, schemas_sale.CreateSale, schemas_sale.BaseSale]
 
     def delete(self, sale_id: int):
         sale_row = self._get(sale_id)
+
+        # Update qty in items
+        sli_rows = self.db.query(tables.SaleLineItem).filter(tables.SaleLineItem.sale_id == sale_id)
+        for pd_sli in sli_rows:
+            item_row = self.db.query(tables.Item).filter(tables.Item.id == pd_sli.item_id)
+            item_pd = schemas_item.Item.from_orm(item_row.first())
+            item_pd.qty += pd_sli.qty
+            item_row.update(item_pd.dict())
+
         sale_row.delete(synchronize_session=False)
         list_rows_sli = self.db.query(tables.SaleLineItem).filter(tables.SaleLineItem.sale_id == sale_id)
         list_rows_sli.delete(synchronize_session=False)
         self.db.commit()
+
+
+if __name__ == '__main__':
+    sli_s = [(1, 3), (2, 2), (4, 20)]
+    new_sli_s = [(2, 4), (3, 2), (4, 20)]
+    items = [(1, 1), (2, 5), (3, 3), (4, 20), (5, 10)]
+    d_sli_s, d_new_sli_s, d_items = dict(sli_s), dict(new_sli_s), dict(items)
+    c_sli_s, c_new_sli_s = Counter(), Counter()
+    for item_id, qty in d_sli_s.items():
+        c_sli_s[item_id] += qty
+    for item_id, qty in d_new_sli_s.items():
+        c_new_sli_s[item_id] += qty
+    item_ids = list(d_sli_s | d_new_sli_s)
+    need_items = {item_id: d_items[item_id] for item_id in item_ids if c_sli_s[item_id] != c_new_sli_s[item_id]}
+    for item_id, qty in need_items.items():
+        qty += c_sli_s[item_id] - c_new_sli_s[item_id]
+        d_items[item_id] = qty
+    print(d_items)
