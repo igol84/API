42863462d2605549e49372b27eae2255227d1a5d
igol84 <igolub84@gmail.com>
2021-12-22 18:01:49 +0200

    header put from sale in items

--- api/crud/header_sale_registration.py
+++ api/crud/header_sale_registration.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import Session
 
 from .. import database
 from .. import tables
-from ..schemas.header_sale_registration import EditSLIPrice
+from ..schemas.header_sale_registration import EditSLIPrice, PutItemToOldSale
 
 
 class HeaderSaleRegistration:
     def __init__(self, db: Session = Depends(database.get_db)):
         self.db = db
 
-    def edit_sli_price(self, request: EditSLIPrice) -> list[tables.SaleLineItem]:
+    def edit_sli_price(self, request: EditSLIPrice) -> None:
         old_sli = request.old_sli
         table = tables.SaleLineItem
         old_row_sli = self.db.query(tables.SaleLineItem).filter(
@@ -20,11 +20,46 @@ class HeaderSaleRegistration:
         )
         if not old_row_sli.first():
             key_m = f'sale_id:{old_sli.sale_id}, item_id:{old_sli.item_id}, sale_price:{old_sli.sale_price}'
-            err_mes = f'{table.__name__} with the {key_m} not available'
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=err_mes)
+            err_mess = f'{table.__name__} with the {key_m} not available'
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=err_mess)
         old_row_sli.delete(synchronize_session=False)
         new_sli_row = table(**request.new_sli.dict())
         self.db.add(new_sli_row)
         self.db.commit()
-        self.db.refresh(new_sli_row)
-        return new_sli_row
+
+    def put_items_from_old_sale(self, request: PutItemToOldSale) -> None:
+        table = tables.SaleLineItem
+        for del_sli in request.list_del_sli:
+            row_sli = self.db.query(table).filter(
+                table.sale_id == del_sli.sale_id,
+                table.item_id == del_sli.item_id,
+                table.sale_price == del_sli.sale_price
+            )
+            if not row_sli.first():
+                key_m = f'sale_id:{del_sli.sale_id}, item_id:{del_sli.item_id}, sale_price:{del_sli.sale_price}'
+                err_mess = f'{table.__name__} with the {key_m} not available'
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=err_mess)
+            row_sli.delete(synchronize_session=False)
+
+        table = tables.Item
+        for pd_item in request.list_new_items:
+            new_item = table(**pd_item.dict())
+            self.db.add(new_item)
+
+        for pd_item in request.list_update_items:
+            row_item = self.db.query(table).filter(table.id == pd_item.id)
+            if not row_item.first():
+                key_m = f'item_id:{pd_item.id}'
+                err_mess = f'{table.__name__} with the {key_m} not available'
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=err_mess)
+            row_item.update(pd_item.dict())
+
+        if request.delete:
+            table = tables.Sale
+            row_sale = self.db.query(table).filter(table.id == request.sale_id)
+            if not row_sale.first():
+                key_m = f'sale_id:{request.sale_id}'
+                err_mess = f'{table.__name__} with the {key_m} not available'
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=err_mess)
+            row_sale.delete(synchronize_session=False)
+        self.db.commit()
--- api/routers/header_sale_registration.py
+++ api/routers/header_sale_registration.py
@@ -8,6 +8,11 @@ router = APIRouter(tags=['HeaderSaleRegistration'], prefix='/header_sale_registr
                    dependencies=[Depends(get_current_user)])
 
 
-@router.put('/edit_sli_price', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.SaleLineItem)
-def edit_sale_price(request: schemas.EditSLIPrice, sale_line_item: crud.HeaderSaleRegistration = Depends()):
-    return sale_line_item.edit_sli_price(request)
+@router.put('/edit_sli_price', status_code=status.HTTP_202_ACCEPTED)
+def edit_sale_price(request: schemas.EditSLIPrice, header_sale_registration: crud.HeaderSaleRegistration = Depends()):
+    header_sale_registration.edit_sli_price(request)
+
+
+@router.put('/put_item_from_old_sale', status_code=status.HTTP_202_ACCEPTED)
+def put_item_to_old_sale(request: schemas.PutItemToOldSale, header: crud.HeaderSaleRegistration = Depends()):
+    header.put_items_from_old_sale(request)
