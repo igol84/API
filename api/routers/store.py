3440fda31245ce616c6587fa508c0c88874cd669
igol84 <igolub84@gmail.com>
2022-02-24 13:45:11 +0200

    in login get store_id this user - fix role checker on store

--- api/routers/store.py
+++ api/routers/store.py
@@ -5,30 +5,35 @@ from .. schemas import store as schemas
 from ..auth2 import RoleChecker
 
 allow_create_resource = RoleChecker(["admin"])
-router = APIRouter(tags=['Stores'], prefix='/store', dependencies=[Depends(allow_create_resource)])
+allow_read_resource = RoleChecker(["admin", "user"])
+router = APIRouter(tags=['Stores'], prefix='/store')
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.StoreWithDetails)
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.StoreWithDetails,
+             dependencies=[Depends(allow_create_resource)])
 def create(request: schemas.CreateStore, crud_store: crud.Store = Depends()):
     return crud_store.create(request)
 
 
-@router.get('/', response_model=list[schemas.StoreWithDetails])
+@router.get('/', response_model=list[schemas.StoreWithDetails],
+             dependencies=[Depends(allow_read_resource)])
 def get_all(skip: int = None, limit: int = None, search: str = None, crud_store: crud.Store = Depends()):
     return crud_store.get_all(skip, limit, search)
 
 
-@router.get('/{store_id}', status_code=200, response_model=schemas.StoreWithDetails)
+@router.get('/{store_id}', status_code=200, response_model=schemas.StoreWithDetails,
+             dependencies=[Depends(allow_read_resource)])
 def show(store_id: int, crud_store: crud.Store = Depends()):
     return crud_store.get(store_id)
 
 
-@router.put('/', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.StoreWithDetails)
+@router.put('/', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.StoreWithDetails,
+             dependencies=[Depends(allow_create_resource)])
 def update(request: schemas.UpdateStore, crud_store: crud.Store = Depends()):
     return crud_store.update(request)
 
 
-@router.delete('/{store_id}')
+@router.delete('/{store_id}', dependencies=[Depends(allow_create_resource)])
 def delete(store_id: int, crud_store: crud.Store = Depends()):
     crud_store.delete(store_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
