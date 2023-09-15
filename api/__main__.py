ee5b41e5abfe55e5181052806359be7ec54c7e96
igol84 <igolub84@gmail.com>
2023-09-15 16:22:47 +0300

    ssl

--- api/__main__.py
+++ api/__main__.py
@@ -4,3 +4,4 @@ from .settings import settings
 
 uvicorn.run('api.app:app', reload=True, host=settings.server_host, port=settings.server_port,
             ssl_keyfile=settings.ssl_keyfile, ssl_certfile=settings.ssl_certfile)
+
