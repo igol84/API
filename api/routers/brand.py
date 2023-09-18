07edd1d65fd7fb330e107be91bea838bdb1a4f15
igol84 <igolub84@gmail.com>
2023-09-18 11:49:49 +0300

    brand open data

--- api/routers/brand.py
+++ api/routers/brand.py
@@ -5,10 +5,10 @@ from ..schemas import brand as schemas
 from ..auth2 import RoleChecker
 
 allow_create_resource = RoleChecker(["admin"])
-router = APIRouter(tags=['Brand'], prefix='/brand', dependencies=[Depends(allow_create_resource)])  #
+router = APIRouter(tags=['Brand'], prefix='/brand')  #
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Brand)
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Brand, dependencies=[Depends(allow_create_resource)])
 def create(request: schemas.CreateBrand, crud_brand: crud.Brand = Depends()):
     return crud_brand.create(request)
 
@@ -28,7 +28,7 @@ def get_images(crud_brand: crud.Brand = Depends()):
     return crud_brand.get_images()
 
 
-@router.post("/image/{brand_id}", status_code=status.HTTP_201_CREATED)
+@router.post("/image/{brand_id}", status_code=status.HTTP_201_CREATED, dependencies=[Depends(allow_create_resource)])
 def create_image(brand_id: int, file: UploadFile, crud_brand: crud.Brand = Depends()):
     crud_brand.save_img(file=file.file, file_name=f'{brand_id}.jpg')
     return {"file_save": 'ok'}
@@ -39,18 +39,18 @@ def show(brand_id: int, crud_brand: crud.Brand = Depends()):
     return crud_brand.get(brand_id)
 
 
-@router.put('/', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Brand)
+@router.put('/', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Brand, dependencies=[Depends(allow_create_resource)])
 def update(request: schemas.UpdateBrand, crud_brand: crud.Brand = Depends()):
     return crud_brand.update(request)
 
 
-@router.delete('/{brand_id}')
+@router.delete('/{brand_id}', dependencies=[Depends(allow_create_resource)])
 def delete(brand_id: int, crud_brand: crud.Brand = Depends()):
     crud_brand.delete(brand_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@router.delete("/image/{brand_id}")
+@router.delete("/image/{brand_id}", dependencies=[Depends(allow_create_resource)])
 def delete_directory(brand_id: int, crud_brand: crud.Brand = Depends()):
     crud_brand.del_img(file_name=f'{brand_id}.jpg')
     return Response(status_code=status.HTTP_204_NO_CONTENT)
